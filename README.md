2d290d8834faf177637900e75a53bc78ddda535a
Rohsin Al Razi <alrazi900@gmail.com>
2023-02-06 02:05:13 +0600

    License & README update

---
+++ README.md
@@ -0,0 +1,34 @@
+# PHP Salary Prediction AI
+[![Minimum PHP Version](https://img.shields.io/badge/php-%3E%3D%207.2-8892BF.svg)](https://php.net/)
+[![Build Status](https://travis-ci.org/php-ai/php-ml.svg?branch=master)](https://travis-ci.org/php-ai/php-ml)
+[![License](https://poser.pugx.org/php-ai/php-ml/license.svg)](https://packagist.org/packages/php-ai/php-ml)
+
+This is a GUI application developed using PHP that predicts the salary of a programmer candidate based on their experience, test score, and interview score. The application uses Least Squares Linear Regression from the PHP-ML library to implement the functionality.
+
+## Features
+
+- User-friendly interface to input the required information
+- Predicts the salary of a programmer candidate based on their experience, test score, and interview score
+- Utilizes the Least Squares Linear Regression algorithm from the PHP-ML library
+
+## Requirements
+
+- PHP 7.2 or higher
+- PHP-ML library
+
+## Usage
+
+1. Clone the repository to your local machine
+2. Install the required dependencies using `composer install` command in project directory
+3. Start the application by opening the `index.php` file in your browser
+4. Input the required information for the candidate and click the "Predict Salary" button
+5. The predicted salary will be displayed on the screen
+6. The dataset is available in the `salary.json` file. You can update the existing dataset according to your needs
+
+## Contributions
+
+Contributions are always welcome. If you would like to contribute to the project, please create a fork of the repository, make your changes, and submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+/**
+ * PHP Salary Prediction AI
+ *
+ * Salary prediction implementation using PHP-ML LeastSquares Linear Regression Algorithm
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2023, wEbCoAdEr
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -81,7 +109,7 @@ if (isset($experience) && isset($test_score) && isset($interview_score)) {
                                         </b>
                                     </div>
                                 <?php } ?>
-                                <button type="submit" class="btn btn-success form-control">Suggest Salary</button>
+                                <button type="submit" class="btn btn-success form-control">Predict Salary</button>
                             </div>
                         </form>
                     </div>
